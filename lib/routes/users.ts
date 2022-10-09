f0c4c72fa1aae4d55e1bfcc957c7635a1820d2c2
gambitier <csegambitier@gmail.com>
2022-10-09 15:30:18 +0530

    add user and userRoles

---
+++ lib/routes/users.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { User } from '../database/models/User';
+// import { UserRole } from '../database/models/UserRole';
+
+export const users = Router();
+
+users.post('/', async (req, res, next) => {
+	try {
+		const user = await User.create(req.body);
+		res.status(201).json(user);
+	} catch (e) {
+		next(e);
+	}
+});
+
+users.get('', async (req, res, next) => {
+	try {
+		res.json({
+			name: 'Dangal',
+		});
+	} catch (e) {
+		next(e);
+	}
+});
+
+users.get('/:id', async (req, res, next) => {
+	try {
+		res.sendStatus(200);
+	} catch (e) {
+		next(e);
+	}
+});
+
+users.put('/:id', async (req, res, next) => {
+	try {
+		res.sendStatus(200);
+	} catch (e) {
+		next(e);
+	}
+});
